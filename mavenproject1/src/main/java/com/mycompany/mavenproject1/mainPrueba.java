9da3473a6ca16c2d5b7fdb186f9b605ac6d1d679
yoxi-caito <56449428+yoxi-caito@users.noreply.github.com>
2019-10-11 10:15:54 -0700

    Add files via upload

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/mainPrueba.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Yona
+ */
+public class mainPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic herep
+        
+        probandoAndo awa = new probandoAndo();
+        
+        awa.print();
+        
+    }
+    
+    
+}
---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/probandoAndo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Yona
+ */
+public class probandoAndo {
+    
+    
+    private String uwu="uwu ";
+    
+    public void print(){
+        for(int i=0;i<6;i++){
+           System.out.println(uwu);
+        }
+        
+    }
+    
+}
